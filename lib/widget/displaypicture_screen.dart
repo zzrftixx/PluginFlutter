309bae36d27aa42768836b4c221734e0445aab6d
zzrftixx <andragiri.opy@gmail.com>
2024-11-05 10:13:15 +0700

    update2

---
+++ lib/widget/displaypicture_screen.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({super.key, required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Display the Picture')),
+      body: Center(
+          child: Image.file(
+        File(imagePath),
+      )),
+    );
+  }
+}
--- lib/widget/takepicture_screen.dart
+++ lib/widget/takepicture_screen.dart
@@ -26,7 +26,7 @@ class TakePictureScreenState extends State<TakePictureScreen> {
       // Get a specific camera from the list of available cameras.
       widget.camera,
       // Define the resolution to use.
-      ResolutionPreset.medium,
+      ResolutionPreset.high,
     );
 
     // Next, initialize the controller. This returns a Future.
@@ -43,6 +43,29 @@ class TakePictureScreenState extends State<TakePictureScreen> {
   @override
   Widget build(BuildContext context) {
     // Fill this out in the next steps.
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Take Picture 362358302103'),
+        ),
+        body: FutureBuilder<void>(
+            future: _initializeControllerFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return CameraPreview(_controller);
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            }),
+        FloatingActionButton(
+          onPressed: () async {
+            try {
+              await _initializeControllerFuture;
+              final image = await _controller.takePicture();
+            } catch (e) {
+              print(e);
+            }
+          },
+          child: const Icon(Icons.camera_alt),
+        ));
   }
 }
